dfc98589d9b64d79af93cb0a6cf8f50eba7fdd7d
Adeshola Ayomide <josephay125d@gmail.com>
2025-05-06 07:22:45 +0100

    card fixed

--- components/NavbarComponent.tsx
+++ components/NavbarComponent.tsx
@@ -88,7 +88,7 @@ export default function NavbarComponent() {
           </Button>
         </NavbarItem>
       </NavbarContent>
-      <div className="px-2 py-4 lg:hidden rounded-sm bg-[#CC2837]/80 hover:bg-[#CC2837] transition duration-300">
+      <div className="px-2 py-3 lg:hidden rounded-sm bg-[#CC2837]/80 hover:bg-[#CC2837] transition duration-300">
       <NavbarMenuToggle
         aria-label={isMenuOpen ? "Close menu" : "Open menu"}
         className="sm:hidden px-2 "
