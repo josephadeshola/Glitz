a8750abaa8d869a15e5d796b793aba5c7a6a68c0
Adeshola Ayomide <josephay125d@gmail.com>
2025-05-09 16:07:42 +0100

    slug route

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -6,7 +6,6 @@ import { FaExpand, FaTimes, FaSpinner, FaDotCircle } from "react-icons/fa";
 import Footer from "@/components/Footer";
 import { notFound } from "next/navigation";
 
-
 type PageProps = {
   params: {
     slug: string;
@@ -18,12 +17,12 @@ export default function Page({ params }: PageProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [loading, setLoading] = useState(true);
 
- if (!product) {
-  return notFound();
-}
+  if (!product) {
+    return notFound();
+  }
 
   return (
-    <div className=" text-gray-800">
+    <div className="text-gray-800">
       <div className="min-h-[60vh] bg-gradient-to-b from-[#111827] via-gray-900 to-white -mt-10 py-20 px-6">
         <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-10 items-center">
           <div className="relative w-full max-w-[500px] h-[500px] mx-auto shadow-xl rounded-full overflow-hidden border-4 border-gray-200">
@@ -31,7 +30,7 @@ export default function Page({ params }: PageProps) {
               data-aos="zoom-in"
               src={product.image}
               alt={product.name}
-              layout="fill"
+              fill
               className="object-cover hover:scale-105 transition-transform duration-300"
             />
           </div>
@@ -59,18 +58,18 @@ export default function Page({ params }: PageProps) {
         </div>
       </div>
 
-      <div className="relative z-10 bg-gradient-to-b from-[#CC2837] via-[#881F2A] rounded-b-lg to-black text-white py-20 px-6">
+      <div className="relative z-10 bg-gradient-to-b from-[#CC2837] via-[#881F2A] to-black rounded-b-lg text-white py-20 px-6">
         <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-16 items-center">
           <div className="space-y-6">
             <h2
               data-aos="zoom-in"
               className="text-3xl md:text-4xl font-bold text-white mb-6"
             >
-              {product.specification.paramiter}
+              {product.specification?.paramiter}
             </h2>
 
             <ul className="space-y-4 text-lg font-medium">
-              {Object.entries(product.specification).map(([key, value]) => {
+              {Object.entries(product.specification || {}).map(([key, value]) => {
                 if (key === "size" && value) {
                   return (
                     <div data-aos="zoom-in" key={key} className="">
@@ -83,7 +82,7 @@ export default function Page({ params }: PageProps) {
                   );
                 }
 
-                if (key !== "size") {
+                if (key !== "size" && key !== "paramiter") {
                   return (
                     <div data-aos="zoom-in" key={key}>
                       <div className="flex gap-3 items-center">
@@ -105,7 +104,7 @@ export default function Page({ params }: PageProps) {
               data-aos="zoom-in"
               src={product.image}
               alt={product.name}
-              layout="fill"
+              fill
               className="object-cover hover:scale-105 transition-transform duration-300"
             />
 
@@ -131,15 +130,19 @@ export default function Page({ params }: PageProps) {
                   <FaSpinner className="animate-spin text-3xl text-gray-600" />
                 </div>
               )}
-              <img
+              <Image
                 src={product.image}
                 alt={product.name}
-                className="object-contain w-full h-full"
+                fill
+                className="object-contain"
                 style={{ transform: "scale(1.5)" }}
                 onLoad={() => setLoading(false)}
               />
               <button
-                onClick={() => setIsModalOpen(false)}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsModalOpen(false);
+                }}
                 className="absolute top-4 right-4 text-white bg-black/70 p-2 rounded-full"
               >
                 <FaTimes size={20} />
