653e29eaa09cf9146b15e89ea942c39f877ad018
Adeshola Ayomide <josephay125d@gmail.com>
2025-05-20 18:26:02 +0100

    whatApp chat box added

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/www.glitz.com.ng/:path*',
-        destination: 'https://glitz.com.ng/:path*',
-        permanent: true,
+    eslint: {
+        ignoreDuringBuilds: true,
       },
-    ];
-  },
 };
 
 module.exports = nextConfig;
+
+
